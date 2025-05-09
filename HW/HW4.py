2c56fc4a50bbe08381e3de0ef34c170212041914
Caleb Eastlund <caleb@dor.uwyo.edu>
2025-05-09 08:44:35 -0600

    Added documentation to color_cut.py and HW4.py

--- HW/HW4.py
+++ HW/HW4.py
@@ -2,17 +2,41 @@
 
 def splendid_function(inp, area = None):
 	"""
+	Given access to data that has (at least) the same columns
+	as a Legacy Survey DR9 sweep file, identifies any quasar
+	candidates in the data based on a series of flag and color-color cuts.
+	Will also print to screen how many candidates were found in total.
+	Optionally, an area (in square degrees) can be passed to determine
+	how many quasar candidates have been found per square degree.
+	If this is the case, the function will instead print to screen how many quasar
+	candidates were found as well as how many were found per square degree.
 	
 	Parameters
 	----------
+	inp : str or astropy.table Table
+		If given a string, the string must be the full directory path to a .fits file
+		containing (at least) the same columns as a Legacy Survey DR9 sweep file.
+
+		If given a table, the table must contain (at least) the same columns
+		as a Legacy Survey DR9 sweep file.
+
+	area : None or float
+		Default is None. If a value is given, it must be an integer or float
+		corresponding to the area in square degrees spanned by the objects
+		in the given data.
 
 	Returns
 	-------
-
+	classif : boolean array
+		A boolean array corresponding to objects in the given data
+		that satisfied all flag and color-color cuts and are likely quasar candidates.
 	"""
 
+	#CTE Importing packages within the function to allow
+	#CTE full of the function use within an independent python shell
 	print('Importing packages...')
 	import time
+	#CTE Starting the timer to see how long this function takes
 	start_time = time.time()
 
 	import numpy as np
@@ -21,8 +45,10 @@ def splendid_function(inp, area = None):
 	from HW.color_cut import get_cols, get_mags, flag_cuts, rW1_gz_cut, W1W2_W3W4_cut
 	from Tasks.week10.classif import line
 
+	#CTE Reading in data
 	print('Reading in data...')
 	if type(inp) == str:
+		#CTE If a directory path is given, open the data and take only relevant columns
 		with fits.open(inp, memmap=True) as dat:
 			fluxes = ['FLUX_G', 'FLUX_R', 'FLUX_Z', 'FLUX_W1', 'FLUX_W2', 'FLUX_W3', 'FLUX_W4']
 			trans = ['MW_TRANSMISSION_G', 'MW_TRANSMISSION_R', 'MW_TRANSMISSION_Z', 'MW_TRANSMISSION_W1', 
@@ -32,13 +58,19 @@ def splendid_function(inp, area = None):
 			columns = fluxes + trans + flags
 			tab = {i : dat[1].data[i] for i in columns}
 	else:
+		#CTE If a table is already given, proceed.
 		tab = inp
 
+	#CTE Making all flag and color-color cuts.
 	print('Making cuts...')
 	cols, fc, cc1, cc2 = W1W2_W3W4_cut(tab)
 
 	print('Compiling classifications...')
+	#CTE Creating a boolean array of False that is the same length
+	#CTE as the given data
 	class_mask = np.zeros_like(fc, dtype=bool)
+
+	#CTE Setting indices that satisfy all cuts to True
 	fc_in = np.where(fc)[0]
 	cc1_in = fc_in[cc1]
 	cc2_in = cc1_in[cc2]
@@ -46,13 +78,16 @@ def splendid_function(inp, area = None):
 
 	classif = class_mask
 
+	#CTE Printing results
 	if area != None:
 		print(f'{sum(classif)} QSO candidates found ({sum(classif)/area} per square degree).')
 	else:
 		print(f'{sum(classif)} QSO candidates found.')
 
+	#CTE Stopping the clock
 	end_time = time.time()
 
+	#CTE Printing the function run time
 	print(f'Total Runtime: {end_time - start_time}.')
 
 	return classif
@@ -64,8 +99,9 @@ if __name__ == '__main__':
 	an astropy table with (at least) the same columns as a Legacy Survey DR9 sweep file\
 	or the full directory path to such a file. The user can optionally give the total area in square\
 	degrees that the survey covers as well. With this information, the module goes through a series of flag\
-	and color-color cuts to classify any QSO candidates within the file. If an area is given, the module\
-	will print how many QSO candidates are found per square degree.')
+	and color-color cuts to classify any QSO candidates within the file. The module will print to screen\
+	how many QSO candidates have been found. If an area is given, the module\
+	will also print how many QSO candidates are found per square degree.')
 	parser.add_argument('path_or_table', 
 	help='What data would you like to use? Provide either an astropy table with (at least)\
 	the same columns as a Legacy Survey DR9 sweep file, or the full directory path\
@@ -76,3 +112,4 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	classification = splendid_function(args.path_or_table, args.area)
+	print(classification)
--- HW/color_cut.py
+++ HW/color_cut.py
@@ -11,39 +11,65 @@ import time
 
 def get_mags(tab):
 	"""
+	Given a table that has (at least) the same columns as
+	a Legacy Survey DR9 sweep file, collects the following
+	(corrected for MW transmission) magnitudes in a dictionary:
+	g, r, z, W1, W2, W3, and W4.
+
 	Parameters
 	----------
-	tab : 
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns
+		as a Legacy Survey DR9 sweep file to collect magnitude information.
 
 	Returns
 	-------
-
+	mag_dict : dictionary
+		A dictionary containing transmission corrected magnitudes
+		for every object in the given table.
 	"""
 
+	#CTE Collecting flux and transmission column names
 	fluxes = ['FLUX_G', 'FLUX_R', 'FLUX_Z', 'FLUX_W1', 'FLUX_W2', 'FLUX_W3', 'FLUX_W4']
 	trans = ['MW_TRANSMISSION_G', 'MW_TRANSMISSION_R', 'MW_TRANSMISSION_Z', 'MW_TRANSMISSION_W1', 'MW_TRANSMISSION_W2', 'MW_TRANSMISSION_W3', 'MW_TRANSMISSION_W4']
 
+	#CTE Creating strings with just the band names (G, R, Z etc.)
 	bands = [i.split('_')[-1] for i in fluxes]
-	ext_corr_flux = [tab[i]/tab[j] for i, j in zip(fluxes, trans)]
-	mags = f_to_m(np.stack(ext_corr_flux))
+
+	#CTE Correcting fluxes (in nanomaggies) for MW transmission
+	corr_flux = [tab[i]/tab[j] for i, j in zip(fluxes, trans)]
+
+	#CTE Converting fluxes to magnitudes
+	mags = f_to_m(np.stack(corr_flux))
 	
+	#CTE Compiling results in a dictionary
 	mag_dict = {f"{j}_MAG":mags[i] for i, j in enumerate(bands)}
 
 	return mag_dict
 
 def get_cols(tab):
 	"""
+	Given a table that has (at least) the same columns
+	as a Legacy Survey DR9 sweep file, collects predetermined
+	colors in a dictionary
+
 	Parameters
 	----------
-	tab : 
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns
+		as a Legacy Survey DR9 sweep file to collect colors from.
 
 	Returns
 	------
-
+	col_dict : dictionary
+		A dictionary containing predetermined colors for every
+		object in the given table.
 	"""
 
+	#CTE Getting magnitudes from the table
 	mags = get_mags(tab)
 
+	#CTE Collecting colors
 	col_dict = {'r-W1':mags['R_MAG']-mags['W1_MAG'], 'g-z':mags['G_MAG']-mags['Z_MAG'], 
 	'W1-W2':mags['W1_MAG']-mags['W2_MAG'], 'W3-W4':mags['W3_MAG']-mags['W4_MAG']}
 
@@ -51,12 +77,21 @@ def get_cols(tab):
 
 def flag_cuts(tab):
 	"""
+	Given a table that has (at least) the same columns as
+	a Legacy Survey DR9 sweep file, goes through a series of flag
+	(or flag adjacent) cuts that have been pre-selected to be quasar markers.
 
 	Parameters
 	----------
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns
+		as a Legacy Survey DR9 sweep file to begin cutting.
 
 	Returns
 	------
+	fc : boolean array
+		A boolean array corresponding to objects that 
+		satisfy the selected quasar markers.
 
 	"""
 
@@ -66,6 +101,9 @@ def flag_cuts(tab):
 	#CTE regardless of whether or not it is a byte string or Unicode string
 	uc_byte_check = np.char.decode(tab['TYPE']) if isinstance(tab['TYPE'][0], bytes) else tab['TYPE']
 
+	#CTE Limiting to r magnitudes brighter than 19
+	#CTE Limiting to only PSF objects
+	#CTE Limiting ALLMASK and WISEMASK to 0 (QSOs tend to have these values)
 	fc = (f_to_m(tab['FLUX_R']) < 19) & (uc_byte_check == 'PSF') & (tab['ALLMASK_G'] == 0) & (
 		tab['ALLMASK_R'] == 0) & (tab['ALLMASK_Z'] == 0) & (tab['WISEMASK_W1'] == 0) & (
 		(tab['WISEMASK_W2'] == 0))
@@ -74,60 +112,138 @@ def flag_cuts(tab):
 
 def rW1_gz_cut(tab):
 	"""
+	Given a table that has (at least) the same columns as
+	a Legacy Survey DR9 sweep file, makes two color-color cuts
+	in r-W1 vs. g-z space based on pre-screened conditions
+	for quasar candidates. 
 	
 	Parameters
 	----------
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns
+		as a Legacy Survey DR9 sweep file to cut for quasars,
 
 	Returns
 	------
+	cols : dictionary
+		A dictionary of colors determined by the get_cols() function
+		for every object in 'tab'
 
+	fc : boolean array
+		A boolean array corresponding to objects in 'tab'
+		that satisfy the flag cuts determined by the
+		flag_cuts() function.
+
+	cc : boolean array
+		A boolean array corresponding to the subset of
+		'True' values in 'fc' that satisfy the cuts
+		in r-W1 vs. g-z space.
 	"""
 
+	#CTE Getting colors for every object
 	cols = get_cols(tab)
+
+	#CTE Making flag cuts for every object
 	fc = flag_cuts(tab)
 
+	#CTE Separating data in to x and y axes
 	x = cols['g-z'][fc]
 	y = cols['r-W1'][fc]
 
+	#CTE Comparing data to two predetermined line cuts
+	#CTE and keeping all objects that are above BOTH lines
 	cc = (y > line(x, 1, -1)) & (y > line(x, -2.5, -0.5))
 
 	return cols, fc, cc
 
 def W1W2_W3W4_cut(tab):
 	"""
+	Given a table that has (at least) the same columns as
+	a Legacy Survey DR9 sweep file, makes a color-color
+	cut in W3-W4 vs. W1-W2 space based on pre-screened
+	conditions for quasar candidates
 
 	Parameters
 	----------
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns as
+		a Legacy Survey DR9 sweep file to make cuts on.
 
 	Returns
 	------
+	cols : dictionary
+		A dictionary of colors determined by the get_cols() function
+		for every object in 'tab'
 
+	fc : boolean array
+		A boolean array corresponding to objects that satisfy
+		the flag cuts determined by the flag_cuts() function
+
+	cc1 : boolean array
+		A boolean array corresponding to the subset of 'True'
+		values in 'fc' that satisfy the cuts in r-W1 vs. g-z space
+		determined by the rW1_gz_cut() function.
+
+	cc2 : boolean array
+		A boolean array corresponding to the subset of 'True'
+		values in 'cc1' that satisfy the cuts in W3-W4 vs. W1-W2
+		space. 
 	"""
 
+	#CTE Getting colors, flag cuts array, and the initial color-color cuts
 	cols, fc, cc1 = rW1_gz_cut(tab)
 
+	#CTE Separating data into x and y axes
 	x = cols['W1-W2'][fc][cc1]
 	y = cols['W3-W4'][fc][cc1]
 
+	#CTE Comparing data to a predetermined line cut
+	#CTE and keeping objects that are above the line.
 	cc2 = y > line(x, -17, -4)
 
 	return cols, fc, cc1, cc2
 
 def splendid_function_v1(tab, area = None):
 	"""
+	Given a table that has (at least) the same columns
+	as a Legacy Survey DR9 sweep file, identifies any quasar
+	candidates in the table based on a series of flag and color-color cuts.
+	Will also print to screen how many candidates were found in total.
+	Optionally, an area (in square degrees) can be passed to determine
+	how many quasar candidates have been found per square degree.
+	If this is the case, the function will instead print to screen how many
+	quasar candidaes were found as well as how many were found per square
+	degree.
 	
 	Parameters
 	----------
+	tab : astropy.table Table or similar
+		A table of some kind, that has (at least) the same columns
+		as a Legacy Survey DR9 sweep file to identify quasar candidates in.
+
+	area : None or int or float
+		Default is None. If a value is given, it must be an integer or float
+		corresponding to the area in square degrees spanned by the objects
+		in 'tab'.
 
 	Returns
 	-------
-
+	classif : boolean array
+		A boolean array corresponding to object in 'tab' that satisfied
+		all flag and color-color cuts and are likely quasar candidates.
 	"""
+
+	#CTE Starting the clock to see how long this function takes
 	start_time = time.time()
 
+	#CTE Going through all flag and color-color cuts
 	cols, fc, cc1, cc2 = W1W2_W3W4_cut(tab)
 
+	#CTE Creating a boolean array of False that is the same
+	#CTE length as 'tab'
 	class_mask = np.zeros_like(fc, dtype=bool)
+
+	#CTE Setting indices where all cuts were satisfied to True
 	fc_in = np.where(fc)[0]
 	cc1_in = fc_in[cc1]
 	cc2_in = cc1_in[cc2]
@@ -135,13 +251,16 @@ def splendid_function_v1(tab, area = None):
 
 	classif = class_mask
 
+	#CTE Printing results
 	if area != None:
 		print(f'{sum(classif)} QSO candidates found ({sum(classif)/area} per square degree).')
 	else:
 		print(f'{sum(classif)} QSO candidates found.')
 
+	#CTE Stopping the clock
 	end_time = time.time()
 
+	#CTE Printing function run time.
 	print(f'Total Runtime: {end_time - start_time}.')
 
 	return classif
